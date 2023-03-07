2ef49db249ac286955d177ece0072d13f15276f5
tracer-ics2017 <tracer@njuics.org>
2023-03-07 16:38:56 +0800

    >  compile 161220000 lun Linux lun-virtual-machine 5.19.0-35-generic #36~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb 17 15:17:25 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:38:56 up  2:08,  1 user,  load average: 0.68, 0.49, 0.40 7e440a615bb2415a7d80f0f999ca201bcfd7ebb7

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -103,16 +103,16 @@ static bool make_token(char *e) {
          */
 
         switch (rules[i].token_type) {
-	  case TKNOTYPE: break;
+	  case TK_NOTYPE: break;
 
           default:
-		strcpy(tokens[nr_token].str, substr_start, substr_len);
+		strncpy(tokens[nr_token].str, substr_start, substr_len);
 		tokens[nr_token].str[substr_len] = '\0';
-		tokens[ne_token].type = rules[i].token_type;
+		tokens[nr_token].type = rules[i].token_type;
 
-		if(tokens[nr_token].type == '*' && (nr_token == 0 || (tokens[nr_token-1].type != TK_10 && tokens[nr_token-1].type != TK_REG && tokens[nr_token-1].type != TK_16)))
+		if(tokens[nr_token].type == '*' && (nr_token == 0 || (tokens[nr_token-1].type != TK_DEC && tokens[nr_token-1].type != TK_REG && tokens[nr_token-1].type != TK_HEX)))
 			tokens[nr_token].type = TK_POINT;
-                   if(tokens[nr_token].type == '-' && (nr_token == 0 || (tokens[nr_token-1].type != TK_10 && tokens[nr_token-1].type != TK_REG && tokens[nr_token-1].type != TK_16)))
+                if(tokens[nr_token].type == '-' && (nr_token == 0 || (tokens[nr_token-1].type != TK_DEC && tokens[nr_token-1].type != TK_REG && tokens[nr_token-1].type != TK_HEX)))
 	                tokens[nr_token].type = TK_NEG;
 		nr_token++;
 		break;
@@ -131,23 +131,6 @@ static bool make_token(char *e) {
   return true;
 }
 
-uint32_t expr(char *e, bool *success) {
-  if (!make_token(e)) {
-    *success = false;
-    return 0;
-  }
-
-  for(int i = 0; i < nr_token; i++)
-	  Log("tokens[%d]: '%s' (%d)", i, tokens[i].str, tokens[i].type);
-
-  /* TODO: Insert codes to evaluate the expression. */
-  
-  uint32_t result = eval(0, nr_token - 1);
-
-  return result;
-}
-
-
 bool check_parentheses(int p ,int q){
   int tag = 0, flag = 1;
   for(int i = p; i <= q; i++){
@@ -165,6 +148,23 @@ bool check_parentheses(int p ,int q){
   if(flag == 0)
 	return false;
   return true;
+}
+
+int op_precedence(int op){
+	if(op == 259 || op == 260) // "==", "!="
+		return 7;
+	else if(op == '+' || op == '-')
+		return 4;
+	else if(op == '*' || op == '/' || op == '%')
+		return 3;
+	else if(op == 261 || op ==263) // TK_NEG, TK_POINT
+		return 2;
+	else if(op == 264) // "&&"
+		return 11;
+	else if(op == 265) // "||"
+		return 12;
+	else
+		return 0;
 }
 
 int dominant_operator(int p, int q) {
@@ -193,27 +193,10 @@ int dominant_operator(int p, int q) {
 	    char dest[255] = "\0";
 	    for(int j = p; j <= q; j++)
 		    strcat(dest, tokens[j].str);
-	    Log("The dominant of %s is tokens[%d] = '%s'", dest, dominanat, tokens[dominant].str);
+	    Log("The dominant of %s is tokens[%d] = '%s'", dest, dominant, tokens[dominant].str);
     }
   }
   return dominant;
-}
-
-int op_precedence(int op){
-	if(op == 259 || op == 260) // "==", "!="
-		return 7;
-	else if(op == '+' || op == '-')
-		return 4;
-	else if(op == '*' || op == '/' || op == '%')
-		return 3;
-	else if(op == 261 || op ==263) // TK_NEG, TK_POINT
-		return 2;
-	else if(op == 264) // "&&"
-		return 11;
-	else if(op == 265) // "||"
-		return 12;
-	else
-		return 0;
 }
 
 uint32_t eval(int p, int q) {
@@ -230,6 +213,131 @@ uint32_t eval(int p, int q) {
       sscanf(tokens[p].str, "%x", &tmp);
       return tmp;
     }
+    else if(tokens[p].type == TK_REG) {
+      char str1[32] = "\0";
+      char *str2;
+      if(strcmp(tokens[p].str, "$EAX") == 0 || strcmp(tokens[p].str, "$eax") == 0){
+	sprintf(str1, "%8x", cpu.eax);
+}
+      else if(strcmp(tokens[p].str, "$EBX") == 0 || strcmp(tokens[p].str, "$ebx") == 0){
+	sprintf(str1, "%8x", cpu.ebx);
+}
+      else if(strcmp(tokens[p].str, "$ECX") == 0 || strcmp(tokens[p].str, "$ecx") == 0){
+	sprintf(str1, "%8x", cpu.ecx);
+}
+      else if(strcmp(tokens[p].str, "$EDX") == 0 || strcmp(tokens[p].str, "$edx") == 0){
+	sprintf(str1, "%8x", cpu.edx);
+}
+      else if(strcmp(tokens[p].str, "$EBP") == 0 || strcmp(tokens[p].str, "$ebp") == 0){
+	sprintf(str1, "%8x", cpu.ebp);
+}
+      else if(strcmp(tokens[p].str, "$ESP") == 0 || strcmp(tokens[p].str, "$esp") == 0){
+	sprintf(str1, "%8x", cpu.esp);
+}
+      else if(strcmp(tokens[p].str, "$ESI") == 0 || strcmp(tokens[p].str, "$esi") == 0){
+	sprintf(str1, "%8x", cpu.esi);
+}
+      else if(strcmp(tokens[p].str, "$EDI") == 0 || strcmp(tokens[p].str, "$edi") == 0){
+	sprintf(str1, "%8x", cpu.edi);
+}
+      else if(strcmp(tokens[p].str, "$AX") == 0 || strcmp(tokens[p].str, "$ax") == 0){
+	sprintf(str1, "%8x", cpu.eax & 0xffff);
+}
+      else if(strcmp(tokens[p].str, "$BX") == 0 || strcmp(tokens[p].str, "$bx") == 0){
+	sprintf(str1, "%8x", cpu.ebx & 0xffff);
+}
+      else if(strcmp(tokens[p].str, "$CX") == 0 || strcmp(tokens[p].str, "$cx") == 0){
+	sprintf(str1, "%8x", cpu.ecx & 0xffff);
+}
+      else if(strcmp(tokens[p].str, "$DX") == 0 || strcmp(tokens[p].str, "$dx") == 0){
+	sprintf(str1, "%8x", cpu.edx & 0xffff);
+}
+      else if(strcmp(tokens[p].str, "$BP") == 0 || strcmp(tokens[p].str, "$bp") == 0){
+	sprintf(str1, "%8x", cpu.ebp & 0xffff);
+}
+      else if(strcmp(tokens[p].str, "$SP") == 0 || strcmp(tokens[p].str, "$sp") == 0){
+	sprintf(str1, "%8x", cpu.esp & 0xffff);
+}
+      else if(strcmp(tokens[p].str, "$SI") == 0 || strcmp(tokens[p].str, "$si") == 0){
+	sprintf(str1, "%8x", cpu.esi & 0xffff);
+}
+      else if(strcmp(tokens[p].str, "$DI") == 0 || strcmp(tokens[p].str, "$di") == 0){
+	sprintf(str1, "%8x", cpu.edi & 0xffff);
+}
+      else if(strcmp(tokens[p].str, "$AH") == 0 || strcmp(tokens[p].str, "$ah") == 0){
+	sprintf(str1, "%8x", (cpu.eax >> 8) & 0xff);
+}
+      else if(strcmp(tokens[p].str, "$AL") == 0 || strcmp(tokens[p].str, "$al") == 0){
+	sprintf(str1, "%8x", cpu.eax & 0xff);
+}
+      else if(strcmp(tokens[p].str, "$BH") == 0 || strcmp(tokens[p].str, "$bh") == 0){
+	sprintf(str1, "%8x", (cpu.ebx >> 8) & 0xff);
+}
+      else if(strcmp(tokens[p].str, "$BL") == 0 || strcmp(tokens[p].str, "$bl") == 0){
+	sprintf(str1, "%8x", cpu.ebx & 0xff);
+}
+      else if(strcmp(tokens[p].str, "$CH") == 0 || strcmp(tokens[p].str, "$ch") == 0){
+	sprintf(str1, "%8x", (cpu.ecx >> 8) & 0xff);
+}
+      else if(strcmp(tokens[p].str, "$CL") == 0 || strcmp(tokens[p].str, "$cl") == 0){
+	sprintf(str1, "%8x", cpu.ecx & 0xff);
+}
+      else if(strcmp(tokens[p].str, "$DH") == 0 || strcmp(tokens[p].str, "$dh") == 0){
+	sprintf(str1, "%8x", (cpu.edx >> 8) & 0xff);
+}
+      else if(strcmp(tokens[p].str, "$DL") == 0 || strcmp(tokens[p].str, "$dl") == 0){
+	sprintf(str1, "%8x", cpu.edx & 0xff);
+}
+      else if(strcmp(tokens[p].str, "$eip") == 0){
+                sprintf(str1, "%8x", cpu.eip);
+}
+      else if(check_parentheses(p, q) == true)
+	return eval(p + 1, q - 1);
+      else {
+	int op = dominant_operator(p, q);
+	uint32_t val1 = eval(p, op - 1);
+	uint32_t val2 = eval(op + 1, q);
+	switch(tokens[op].type) {
+	  case '+': return val1 + val2;
+	  case '-': return val1 - val2;
+	  case '*': return val1 * val2;
+	  case '/': 
+		if(val2 == 0)
+		  panic("Error: divide zero!");
+		else
+		  return val1 / val2;
+	  case '%':
+		if(val2 == 2)
+		  panic("Error: mode zero!");
+		else
+		  return val1 % val2;
+	  case 257: return (val1 == val2);
+	  case 258: return (val1 != val2);
+	  case 262: return -val2;
+	  case 263: 
+		vaddr_t addr = val2;
+		return vaddr_read(addr, 1);
+	  case 264: return (val1 && val2);
+	  case 265: return (val1 || val2);
+	  default: panic("Error: tokens[%d]=%s, val1=%d, val2=%d\n", op, tokens[op].str, val1, val2);
+	}
+      }
+    }
+  }
+}
 
+uint32_t expr(char *e, bool *success) {
+  if (!make_token(e)) {
+    *success = false;
+    return 0;
+  }
 
+  for(int i = 0; i < nr_token; i++)
+	  Log("tokens[%d]: '%s' (%d)", i, tokens[i].str, tokens[i].type);
+
+  /* TODO: Insert codes to evaluate the expression. */
+  
+  uint32_t result = eval(0, nr_token - 1);
+
+  return result;
 }
