49009750039dfe5012750992876b9abf4c2a79c5
tracer-ics2017 <tracer@njuics.org>
2023-03-31 14:38:53 +0800

    >  compile 2010699 lun Linux lun-virtual-machine 5.19.0-38-generic #39~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 17 21:16:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:38:53 up 42 min,  1 user,  load average: 0.32, 0.53, 0.53 330ecf4cd67580911b8d8354e40b1692808bd19

--- nemu/src/cpu/exec/exec.c
+++ nemu/src/cpu/exec/exec.c
@@ -51,18 +51,18 @@ make_group(gp2,
 
   /* 0xf6, 0xf7 */
 make_group(gp3,
-    EMPTY, EMPTY, EX(not), EX(neg),
+    IDEX(test_I, test), EMPTY, EX(not), EX(neg),
     EX(mul), EX(imul1), EX(div), EX(idiv))
 
   /* 0xfe */
 make_group(gp4,
-    EMPTY, EMPTY, EMPTY, EMPTY,
+    EMPTY, EX(dec), EMPTY, EMPTY,
     EMPTY, EMPTY, EMPTY, EMPTY)
 
   /* 0xff */
 make_group(gp5,
-    EX(inc), EX(dec), EX(call_rm), EMPTY,
-    EX(jmp_rm), EMPTY, EX(push), EMPTY)
+    EXW(inc, 2), EX(dec), EXW(call_rm, 2), EMPTY,
+    EXW(jmp_rm, 2), EMPTY, EX(push), EMPTY)
 
   /* 0x0f 0x01*/
 make_group(gp7,
