f54b309eff3f151468bdd7cb2d1964c64b3e72a7
tracer-ics2017 <tracer@njuics.org>
2023-03-31 14:20:46 +0800

    >  compile 2010699 lun Linux lun-virtual-machine 5.19.0-38-generic #39~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 17 21:16:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:20:46 up 24 min,  1 user,  load average: 0.41, 0.40, 0.37 ca0b5f232d23c26294362da9ee2c7519e57e7e36

--- nemu/src/cpu/exec/data-mov.c
+++ nemu/src/cpu/exec/data-mov.c
@@ -19,13 +19,83 @@ make_EHelper(pop) {
 }
 
 make_EHelper(pusha) {
-  TODO();
+  if (decoding.is_operand_size_16) {
+    t0 = cpu.esp;
+    rtl_lr_w(&t1, R_AX);
+    rtl_push(&t1);
+    rtl_lr_w(&t1, R_CX);
+    rtl_push(&t1);
+    rtl_lr_w(&t1, R_DX);
+    rtl_push(&t1);
+    rtl_lr_w(&t1, R_BX);
+    rtl_push(&t1);
+    rtl_push(&t0);
+    rtl_lr_w(&t1, R_BP);
+    rtl_push(&t1);
+    rtl_lr_w(&t1, R_SI);
+    rtl_push(&t1);
+    rtl_lr_w(&t1, R_DI);
+    rtl_push(&t1);
+  } else {
+    t0 = cpu.esp;
+    rtl_lr_l(&t1, R_EAX);
+    rtl_push(&t1);
+    rtl_lr_l(&t1, R_ECX);
+    rtl_push(&t1);
+    rtl_lr_l(&t1, R_EDX);
+    rtl_push(&t1);
+    rtl_lr_l(&t1, R_EBX);
+    rtl_push(&t1);
+    rtl_push(&t0);
+    rtl_lr_l(&t1, R_EBP);
+    rtl_push(&t1);
+    rtl_lr_l(&t1, R_ESI);
+    rtl_push(&t1);
+    rtl_lr_l(&t1, R_EDI);
+    rtl_push(&t1);
+  }
 
   print_asm("pusha");
 }
 
 make_EHelper(popa) {
-  TODO();
+  if (decoding.is_operand_size_16) {
+    rtl_pop(&t1);
+    rtl_sr_w(R_DI, &t1);
+    rtl_pop(&t1);
+    rtl_sr_w(R_SI, &t1);
+    rtl_pop(&t1);
+    rtl_sr_w(R_BP, &t1);
+
+    cpu.esp += 4;
+
+    rtl_pop(&t1);
+    rtl_sr_w(R_BX, &t1);
+    rtl_pop(&t1);
+    rtl_sr_w(R_DX, &t1);
+    rtl_pop(&t1);
+    rtl_sr_w(R_CX, &t1);
+    rtl_pop(&t1);
+    rtl_sr_w(R_AX, &t1);
+  } else {
+    rtl_pop(&t1);
+    rtl_sr_l(R_EDI, &t1);
+    rtl_pop(&t1);
+    rtl_sr_l(R_ESI, &t1);
+    rtl_pop(&t1);
+    rtl_sr_l(R_EBP, &t1);
+
+    cpu.esp += 4;
+
+    rtl_pop(&t1);
+    rtl_sr_l(R_EBX, &t1);
+    rtl_pop(&t1);
+    rtl_sr_l(R_EDX, &t1);
+    rtl_pop(&t1);
+    rtl_sr_l(R_ECX, &t1);
+    rtl_pop(&t1);
+    rtl_sr_l(R_EAX, &t1);
+  }
 
   print_asm("popa");
 }
@@ -54,10 +124,14 @@ make_EHelper(cltd) {
 
 make_EHelper(cwtl) {
   if (decoding.is_operand_size_16) {
-    TODO();
+    rtl_lr_b(&t0, R_AX);
+    rtl_sext(&t0, &t0, 1);
+    rtl_sr_w(R_AX, &t0);
   }
   else {
-    TODO();
+    rtl_lr_w(&t0, R_AX);
+    rtl_sext(&t0, &t0, 2);
+    rtl_sr_l(R_EAX, &t0);
   }
 
   print_asm(decoding.is_operand_size_16 ? "cbtw" : "cwtl");
