0c4a66ba83a31dbd5093a2855a247728b098c33c
tracer-ics2017 <tracer@njuics.org>
2023-04-01 14:27:47 +0800

    >  run 2010699 lun Linux lun-virtual-machine 5.19.0-38-generic #39~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 17 21:16:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:27:47 up  3:12,  1 user,  load average: 2.00, 1.24, 0.63 297a68d0d0c831b6c252cd86d256e3148beef07d

--- nexus-am/tests/cputest/tests/hello-str.c
+++ nexus-am/tests/cputest/tests/hello-str.c
@@ -4,11 +4,10 @@ char buf[128];
 
 int main() {
 	sprintf(buf, "%s", "Hello world!\n");
-	printf("%d\n", strcmp(buf, "Hello world!\n"));
+	//printf("%d\n", strcmp(buf, "Hello world!\n"));
 	nemu_assert(strcmp(buf, "Hello world!\n") == 0);
 
 	sprintf(buf, "%d + %d = %d\n", 1, 1, 2);
-	printf("%s\n", buf);
 	nemu_assert(strcmp(buf, "1 + 1 = 2\n") == 0);
 
 	sprintf(buf, "%d + %d = %d\n", 2, 10, 12);
