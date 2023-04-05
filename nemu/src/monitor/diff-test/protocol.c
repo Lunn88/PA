07dea18ba8e2c82cad871059c308bceaa3bd3085
tracer-ics2017 <tracer@njuics.org>
2023-04-05 21:11:56 +0800

    >  compile 2010699 lun Linux lun-virtual-machine 5.19.0-38-generic #39~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 17 21:16:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:11:56 up  1:42,  1 user,  load average: 0.89, 1.08, 1.04 41dc5f0ff4f8c07ede79d96a2a1f387b24f15e0d

--- nemu/src/monitor/diff-test/protocol.c
+++ nemu/src/monitor/diff-test/protocol.c
@@ -116,7 +116,6 @@ struct gdb_conn* gdb_begin_inet(const char *addr, uint16_t port) {
     err(1, "socket");
   if (connect(fd, (const struct sockaddr *)&sa, sizeof(sa)) != 0) {
     close(fd);
-    printf("77777\n");
     return NULL;
   }
 
