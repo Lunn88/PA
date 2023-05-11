d9cb4ced69f6408b12408e57fcebd625a6a8508c
tracer-ics2017 <tracer@njuics.org>
2023-05-11 01:38:32 -0700

    >  compile 2010137 sokoko Linux ubuntu 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  01:38:32 up  1:57,  1 user,  load average: 0.18, 0.12, 0.06 cde9772d3f3ffe4ecaa46381bc60a5fa74305617

--- nemu/src/cpu/exec/logic.c
+++ nemu/src/cpu/exec/logic.c
@@ -2,8 +2,8 @@
 
 make_EHelper(test) {
   //TODO();
-  rtl_and(&t1, &id_dest->val, &id_src->val);
-  rtl_update_ZFSF(&t1, id_dest->width);
+  rtl_and(&t0, &id_dest->val, &id_src->val);
+  rtl_update_ZFSF(&t0, id_dest->width);
   rtl_set_CF(&tzero);
   rtl_set_OF(&tzero);
   print_asm_template2(test);
