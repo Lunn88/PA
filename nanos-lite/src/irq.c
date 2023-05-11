8d717cd072a9cd1ba578bd9917123355127b296a
tracer-ics2017 <tracer@njuics.org>
2023-05-10 19:31:57 -0700

    >  run 2010137 sokoko Linux ubuntu 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  19:31:57 up 39 min,  1 user,  load average: 0.09, 0.11, 0.07 fd11fc4446394d76779bb1c2280936235bbc265d

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -1,7 +1,9 @@
 #include "common.h"
 
 extern _RegSet* do_syscall(_RegSet* r);
+
 static _RegSet* do_event(_Event e, _RegSet* r) {
+printf("%d\n", _EVENT_SYSCALL);
     switch (e.event) {
         case _EVENT_SYSCALL:
             do_syscall(r); 
