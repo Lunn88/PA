39e0086f16662f739aaca59784c639554dc42ca9
tracer-ics2017 <tracer@njuics.org>
2023-05-25 07:22:27 -0700

    >  run 2010137 sokoko Linux ubuntu 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  07:22:27 up  4:41,  1 user,  load average: 1.16, 0.85, 0.51 7f847de920e0ba5034ada0386cd28ffcb5feb9da

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -4,15 +4,14 @@ extern _RegSet* do_syscall(_RegSet* r);
 extern _RegSet* schedule(_RegSet* prev);
 
 static _RegSet* do_event(_Event e, _RegSet* r) {
-  _RegSet * ret = NULL;
     switch (e.event) {
         case _EVENT_SYSCALL: do_syscall(r); break;
-	case _EVENT_TRAP: ret = schedule(r); break;
-	case _EVENT_IRQ_TIME: ret = schedule(r); break;
+	case _EVENT_TRAP: return schedule(r);
+	case _EVENT_IRQ_TIME: return schedule(r);
         default: panic("Unhandled event ID = %d", e.event);
     }
 
-  return ret;
+  return NULL;
 }
 
 void init_irq(void) {
