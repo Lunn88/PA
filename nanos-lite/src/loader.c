741229a3696bb2808579a39baee480978d0e74fa
tracer-ics2017 <tracer@njuics.org>
2023-05-25 00:07:40 -0700

    >  run 2010137 sokoko Linux ubuntu 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  00:07:40 up  4:37,  1 user,  load average: 0.96, 0.97, 0.61 ff568941f7a957e4ca08c221e86e284b6ce4ca8

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -6,10 +6,18 @@
 extern size_t get_ramdisk_size();
 extern void ramdisk_read(void *buf, off_t offset, size_t len);
 
+void *new_page();
 uintptr_t loader(_Protect *as, const char *filename) {
-    int fd = fs_open(filename, 0, 0);
-    size_t f_size = fs_filesz(fd);
-    fs_read(fd, DEFAULT_ENTRY, f_size);
-    fs_close(fd);
-    return (uintptr_t)DEFAULT_ENTRY;
+  int fd = fs_open(filename, 0, 0);
+  size_t f_size = fs_filesz(fd);
+  void *fz_end = DEFAULT_ENTRY + f_size;
+  void *va, *pa;
+  for(va = DEFAULT_ENTRY; va < fz_end; va += PGSIZE){
+    pa = new_page();
+    Log("Map va to pa: 0x%08x to 0x%08x", va, pa);
+    _map(as, va, pa);
+    fs_read(fd, pa, (fz_end - va) < PGSIZE ? (fz_end - va) : PGSIZE);
+  }
+  fs_close(fd);
+  return (uintptr_t)DEFAULT_ENTRY;
 }
